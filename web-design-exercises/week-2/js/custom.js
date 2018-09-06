a4bb8edd771f22cae94f93100afe01199840b981
Min Chen <min75208@gmail.com>
2018-09-06 19:08:10 -0400

    upload week 2 assignment

---
+++ web-design-exercises/week-2/js/custom.js
@@ -0,0 +1,15 @@
+var person = "my brother";
+var locations = ['Boston', 'beach'];
+var activities = ['relax', 'whales', 'walk', 'ocean'];
+var food = ['seafood', 'lobster'];
+
+document.getElementById('person').textContent = person;
+document.getElementById('location').textContent = locations[0];
+document.getElementById('secondLocation').textContent = locations[1];
+document.getElementById('weather').textContent = 'Nice';
+document.getElementById('activity').textContent = activities[0];
+document.getElementById('secondActivity').textContent = activities[1];
+document.getElementById('food').textContent = food[0];
+document.getElementById('thirdActivity').textContent = activities[2];
+document.getElementById('fourthActivity').textContent = activities[3];
+document.getElementById('feeling').textContent = 'wonderful';
