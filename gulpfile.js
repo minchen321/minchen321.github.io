c93c69d35257c192142c23d54ee0fad12cca9c0e
Min Chen <min75208@gmail.com>
2019-06-30 01:09:26 -0400

    Added browser autoprefix to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('autoprefixer');
 const minifyCSS = require('gulp-csso');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
 
 gulp.task('sass', function(){
   return gulp.src('src/sass/main.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(minifyCSS())
-    .pipe(concat('main.css'))
+    .pipe(concat('main.min.css'))
+    .pipe(postcss([autoprefixer('last 2 versions')]))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist/css'))
 });
 
